a6dbc38082d05b2b72908647943b922affe5b0a1
sarahdong <dong.xiaoxu@northeastern.edu>
2024-01-24 12:45:46 -0800

    add simple card

---
+++ trello.py
@@ -0,0 +1,50 @@
+import requests
+import sys
+
+# Constants
+API_KEY = 'YOUR_API_KEY'
+TOKEN = 'YOUR_TOKEN'
+BASE_URL = 'https://api.trello.com/1/'
+
+def create_card(board_id, list_id, card_name, card_desc, labels, comment):
+    url = f"{BASE_URL}cards"
+    query = {
+        'key': API_KEY,
+        'token': TOKEN,
+        'idList': list_id,
+        'name': card_name,
+        'desc': card_desc,
+        'idLabels': labels
+    }
+    response = requests.post(url, params=query)
+    card = response.json()
+
+    # Add comment to the card
+    if comment:
+        comment_url = f"{BASE_URL}cards/{card['id']}/actions/comments"
+        comment_query = {
+            'key': API_KEY,
+            'token': TOKEN,
+            'text': comment
+        }
+        requests.post(comment_url, params=comment_query)
+
+    return card
+
+def main():
+    if len(sys.argv) < 6:
+        print("Usage: python trello_cli.py <board_id> <list_id> <card_name> <card_desc> <labels_comma_separated> [comment]")
+        sys.exit(1)
+
+    board_id = sys.argv[1]
+    list_id = sys.argv[2]
+    card_name = sys.argv[3]
+    card_desc = sys.argv[4]
+    labels = sys.argv[5].split(',')
+    comment = sys.argv[6] if len(sys.argv) > 6 else ''
+
+    card = create_card(board_id, list_id, card_name, card_desc, labels, comment)
+    print(f"Card created successfully. Card ID: {card['id']}")
+
+if __name__ == "__main__":
+    main()
